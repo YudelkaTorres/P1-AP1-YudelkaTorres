60c0fe22eb47bfbf1c71e94e0515074a1c6f4935
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-09-22 20:35:21 -0400

    Aplicando el File scoped namespace.

--- Program.cs
+++ Program.cs
@@ -1,36 +1,41 @@
 using P1_AP1_YudelkaTorres.Components;
+using P1_AP1_YudelkaTorres.DAL;
+using Microsoft.EntityFrameworkCore;
 
-namespace P1_AP1_YudelkaTorres
+namespace P1_AP1_YudelkaTorres;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var ConnectionString = builder.Configuration.GetConnectionString("SqlConstr");
+
+        builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConnectionString));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAntiforgery();
-
-            app.MapStaticAssets();
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
     }
 }
