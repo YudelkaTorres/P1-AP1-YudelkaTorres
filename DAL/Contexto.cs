1d76710b3927a6b230f9746d83996b014cdfa19f
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-09-29 18:47:20 -0400

    Renombrando de Registro a EntradasHuacales.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -5,5 +5,5 @@ namespace P1_AP1_YudelkaTorres.DAL;
 public class Contexto : DbContext
 {
 	public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-	public DbSet<Registro> Registro { get; set; }
+	public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
 }
