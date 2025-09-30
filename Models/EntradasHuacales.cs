77c5db59c65a70ad67f6bcc6a31b7da0ebc54c26
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-09-29 21:03:36 -0400

    Creando el modelo.

--- Models/EntradasHuacales.cs
+++ Models/EntradasHuacales.cs
@@ -4,5 +4,17 @@ namespace P1_AP1_YudelkaTorres.Models;
 public class EntradasHuacales
 {
 	[Key]
-	public int Id { get; set; }
+	public int EntradaId { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten caracteres especiales")]
+    [Required(ErrorMessage = "El cliente es obligatorio")]
+    public string NombreCliente { get; set; } = string.Empty;
+
+    [Required]
+    [Range(0.0, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 1")]
+    public int Cantidad { get; set; }
+
+    [Required]
+    [Range(0.0, double.MaxValue, ErrorMessage = "El precio debe ser mayor o igual a 0")]
+    public decimal Precio { get; set; }
 }
--- Models/Registro.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace P1_AP1_YudelkaTorres.Models;
-public class Registro
-{
-	[Key]
-	public int Id { get; set; }
-}
